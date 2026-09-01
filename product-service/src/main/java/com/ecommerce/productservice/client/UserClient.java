9b0491bed666c62fe3a49082ba3de4b65f9f82e8
ayoubnachti <ayoubstudies01@gmail.com>
2026-09-01 18:52:04 +0100

    feat: add circuit breaker + fallback for user-service calls, load testing tooling

--- product-service/src/main/java/com/ecommerce/productservice/client/UserClient.java
+++ product-service/src/main/java/com/ecommerce/productservice/client/UserClient.java
@@ -1,23 +1,36 @@
 package com.ecommerce.productservice.client;
 
+import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.resilience.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
 import com.ecommerce.productservice.dtos.UserResponse;
 
+
 @Service
 public class UserClient {
   private final RestClient restClient;
+  private final CircuitBreaker circuitBreaker;
 
-  public UserClient(@LoadBalanced RestClient.Builder loadBalanced) {
+  public UserClient(@LoadBalanced RestClient.Builder loadBalanced, CircuitBreakerFactory circuitBreakerFactory) {
     this.restClient = loadBalanced.baseUrl("http://user-service").build();
+    this.circuitBreaker = circuitBreakerFactory.create("userService");
   }
 
+  @Retryable(maxRetries = 3)
   public UserResponse getSeller(String id) {
-    return this.restClient.get()
+    return circuitBreaker.run(
+      ()-> this.restClient.get()
         .uri("/users/{id}", id)
         .retrieve()
-        .body(UserResponse.class);
+        .body(UserResponse.class),
+      throwable -> getSellerFallback(id));
+  }
+
+  private UserResponse getSellerFallback(String id) {
+    return new UserResponse(id, "Can't resolve this user");
   }
 }
