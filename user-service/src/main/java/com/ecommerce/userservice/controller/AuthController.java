52f8e7ca7cf1d3a3731fbb3df4e116097e01a889
Jaouhar-Yossef <youssefjoauhar911@gmail.com>
2026-09-06 14:11:04 +0100

    feat(user-service): implement login with JWT

---
+++ user-service/src/main/java/com/ecommerce/userservice/controller/AuthController.java
@@ -0,0 +1,25 @@
+package com.ecommerce.userservice.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ecommerce.userservice.dto.LoginRequest;
+import com.ecommerce.userservice.service.AuthService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+}
