ebc4fc7eacfd6feccc0e0c51e52d75d74140b1c4
heeemzaaa <hamzaelkhawlani00@gmail.com>
2026-09-18 13:09:26 +0100

    ensuring the role of the requester

--- media-service/src/main/java/com/ecommerce/mediaservice/controllers/MediaController.java
+++ media-service/src/main/java/com/ecommerce/mediaservice/controllers/MediaController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import com.ecommerce.mediaservice.common.ResponseData;
 import com.ecommerce.mediaservice.services.MediaService;
@@ -19,8 +20,10 @@ import lombok.RequiredArgsConstructor;
 public class MediaController {
     public final MediaService mediaService;
 
+    @PreAuthorize("hasRole('SELLER')")
     @PostMapping(consumes = "multipart/form-data")
-    public ResponseEntity<ResponseData<String>> saveMedia(@PathVariable String productId, @RequestParam(required = false) MultipartFile[] images) {
+    public ResponseEntity<ResponseData<String>> saveMedia(@PathVariable String productId,
+            @RequestParam(required = false) MultipartFile[] images) {
         return ResponseEntity.ok(mediaService.saveMedia(productId, images));
     }
 }
