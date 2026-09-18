0313f9faa112502aa881c6beaa1ec07a2764f4d7
heeemzaaa <hamzaelkhawlani00@gmail.com>
2026-09-18 14:02:46 +0100

    delete unnecessary imports

--- media-service/src/main/java/com/ecommerce/mediaservice/services/MediaService.java
+++ media-service/src/main/java/com/ecommerce/mediaservice/services/MediaService.java
@@ -1,8 +1,6 @@
 package com.ecommerce.mediaservice.services;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 import javax.imageio.ImageIO;
